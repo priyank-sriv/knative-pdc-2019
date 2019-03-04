5bcf5e65e0bec44edaed0d6703d087d5ed8aa7d2
priyank-sriv <priyank.srivastava@appdirect.com>
2019-03-04 16:11:53 +0530

    cleanup gcp code for file handling

--- parser/src/main/java/com/appdirect/demo/functions/content/parser/ParserFunction.java
+++ parser/src/main/java/com/appdirect/demo/functions/content/parser/ParserFunction.java
@@ -1,20 +1,20 @@
 package com.appdirect.demo.functions.content.parser;
 
 import static org.slf4j.LoggerFactory.getLogger;
-import static org.springframework.cloud.gcp.storage.GoogleStorageProtocolResolver.PROTOCOL;
 
 import com.appdirect.demo.functions.domain.bo.CsvParserConfig;
 import com.appdirect.demo.functions.domain.bo.RawEvent;
 import com.appdirect.demo.functions.domain.bo.UploadNotification;
 import com.appdirect.demo.functions.domain.exception.ParserException;
 import com.appdirect.demo.functions.domain.exception.ParserIOException;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -25,8 +25,6 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gcp.storage.GoogleStorageResource;
-import org.springframework.core.io.Resource;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
@@ -47,7 +45,7 @@ public class ParserFunction implements Function<UploadNotification, RawEvent> {
   public RawEvent apply(UploadNotification notif) {
 
     try {
-      Resource gcsFile = gcsFileResource(notif.getBucketId(), notif.getObjectId());
+      Blob gcsFile = gcsFile(notif.getBucketId(), notif.getObjectId());
       Path dest = downloadLocal(gcsFile);
       removeRemote(gcsFile);
 
@@ -58,8 +56,8 @@ public class ParserFunction implements Function<UploadNotification, RawEvent> {
 
       // todo: stream the response and send each message to channel
 
-    } catch (URISyntaxException e) {
-      throw new ParserException("Invalid resource identifier", e);
+    } catch (StorageException e) {
+      throw new ParserException("Storage API error", e);
     } catch (IOException e) {
       throw new ParserIOException("IO error in parsing", e);
     }
@@ -69,27 +67,26 @@ public class ParserFunction implements Function<UploadNotification, RawEvent> {
 
   //......##### internal #####......//
 
-  
+
   private Function<? extends String[], RawEvent> mapper(CsvParserConfig parserConfig) {
     return r -> RawEvent.builder().referenceId(UUID.randomUUID().toString()).build();
   }
 
-  private Path downloadLocal(Resource gcsFile) throws IOException {
-    Path dest = Paths.get(System.getProperty("java.io.tmpdir"), gcsFile.getFilename());
-    ((GoogleStorageResource) gcsFile).getBlob().downloadTo(dest);
+  private Path downloadLocal(Blob gcsFile) throws StorageException {
+    Path dest = Paths.get(System.getProperty("java.io.tmpdir"), gcsFile.getBlobId().getName());
+    gcsFile.downloadTo(dest);
     return dest;
   }
 
-  private void removeRemote(Resource gcsFile) throws IOException {
-    boolean deleted = storage.delete(((GoogleStorageResource) gcsFile).getBlob().getBlobId());
+  private void removeRemote(Blob gcsFile) throws StorageException {
+    boolean deleted = gcsFile.delete();
     if (!deleted) {
       LOGGER.error("Unable to delete remote file in google storage");
     }
   }
 
-  private Resource gcsFileResource(String bucketId, String objectId) throws URISyntaxException {
-    String location = new URI(PROTOCOL, bucketId, objectId, null, null).toString();
-    return new GoogleStorageResource(this.storage, location);
+  private Blob gcsFile(String bucketId, String objectId) throws StorageException {
+    return storage.get(BlobId.of(bucketId, objectId));
   }
 
   private CsvParserConfig parserConfig() {
